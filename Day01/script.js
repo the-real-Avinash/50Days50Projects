a409537307a0b293e024ab815c85a6c191d7f1ac
insider-avinash <gavandheavinash12@gmail.com>
2023-01-22 23:38:33 +0530

    intial commit

---
+++ Day01/script.js
@@ -0,0 +1,13 @@
+const panels = document.querySelectorAll(".panel");
+panels.forEach((panels) => {
+  panels.addEventListener("click", () => {
+    removeActiveClasses();
+    panels.classList.add("active");
+  });
+});
+
+const removeActiveClasses = () => {
+  panels.forEach((panels) => {
+    panels.classList.remove("active");
+  });
+};
